f17be81fc2626e433b23344c0d64fc1c2882a050
LeonardoSSev <leonardossev@gmail.com>
2021-02-09 14:33:04 -0300

    config: Configures microservice with RMQ transport

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+require('dotenv').config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Main');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL],
+      queue: process.env.RABBITMQ_QUEUE
+    }
+  });
+
+  
+
+  await app.listen(() => logger.log('Microservice is listening!'));
 }
+
 bootstrap();
